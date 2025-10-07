430738a2a77c8da6ab128e1eba02a4389a39b323
RoboRace <roborace@sirious.nl>
2025-10-07 18:02:57 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -106,31 +106,37 @@ function IR_action () {
     if (IRcode == Afstandsbediening.een()) {
         herhaal = 1
         basic.showString("1x")
+    } else {
+        if (IRcode == Afstandsbediening.twee()) {
+            herhaal = 2
+            basic.showString("2x")
+        } else {
+            if (IRcode == Afstandsbediening.drie()) {
+                herhaal = 3
+                basic.showString("3x")
+            } else {
+                if (IRcode == Afstandsbediening.vier()) {
+                    herhaal = 4
+                    basic.showString("4x")
+                } else {
+                    if (IRcode == Afstandsbediening.vijf()) {
+                        herhaal = 5
+                        basic.showString("5x")
+                    } else {
+                        if (IRcode == Afstandsbediening.op()) {
+                            basic.showArrow(ArrowNames.North)
+                            Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Green)
+                            Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
+                            basic.pause(herhaal * 500)
+                            Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+                            Maqueen_V5.setRgbOff(Maqueen_V5.DirectionType.All)
+                        }
+                    }
+                }
+            }
+        }
     }
-    if (IRcode == Afstandsbediening.twee()) {
-        herhaal = 2
-        basic.showString("2x")
-    }
-    if (IRcode == Afstandsbediening.drie()) {
-        herhaal = 3
-        basic.showString("3x")
-    }
-    if (IRcode == Afstandsbediening.vier()) {
-        herhaal = 4
-        basic.showString("4x")
-    }
-    if (IRcode == Afstandsbediening.vijf()) {
-        herhaal = 5
-        basic.showString("5x")
-    }
-    if (IRcode == Afstandsbediening.op()) {
-        basic.showArrow(ArrowNames.North)
-        Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Green)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.All, Maqueen_V5.Dir.CW, 100)
-        basic.pause(herhaal * 500)
-        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
-        Maqueen_V5.setRgbOff(Maqueen_V5.DirectionType.All)
-    }
+    // Split to limit nest depth
     if (IRcode == Afstandsbediening.neer()) {
         basic.showArrow(ArrowNames.South)
         Maqueen_V5.setRgblLed(Maqueen_V5.DirectionType.All, Maqueen_V5.CarLightColors.Blue)
@@ -138,40 +144,45 @@ function IR_action () {
         basic.pause(herhaal * 500)
         Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
         Maqueen_V5.setRgbOff(Maqueen_V5.DirectionType.All)
+    } else {
+        if (IRcode == Afstandsbediening.links()) {
+            basic.showArrow(ArrowNames.East)
+            richtingaanwijzer(0)
+            Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
+            basic.pause(herhaal * 500)
+            Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+        } else {
+            if (IRcode == Afstandsbediening.rechts()) {
+                basic.showArrow(ArrowNames.West)
+                richtingaanwijzer(1)
+                Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+                basic.pause(herhaal * 500)
+                Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+            } else {
+                if (IRcode == Afstandsbediening.ster()) {
+                    basic.showArrow(ArrowNames.NorthWest)
+                    Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 100)
+                    Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
+                    basic.pause(herhaal * 500)
+                    Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+                } else {
+                    if (IRcode == Afstandsbediening.hekje()) {
+                        basic.showArrow(ArrowNames.NorthEast)
+                        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
+                        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 100)
+                        basic.pause(herhaal * 500)
+                        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
+                    } else {
+                        if (IRcode == Afstandsbediening.ok()) {
+                            basic.showIcon(IconNames.Yes)
+                            music.play(music.stringPlayable("C5 A F F E G A B ", 264), music.PlaybackMode.UntilDone)
+                        }
+                    }
+                }
+            }
+        }
+        basic.showIcon(IconNames.Diamond)
     }
-    if (IRcode == Afstandsbediening.links()) {
-        basic.showArrow(ArrowNames.East)
-        richtingaanwijzer(0)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
-        basic.pause(herhaal * 500)
-        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
-    }
-    if (IRcode == Afstandsbediening.rechts()) {
-        basic.showArrow(ArrowNames.West)
-        richtingaanwijzer(1)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
-        basic.pause(herhaal * 500)
-        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
-    }
-    if (IRcode == Afstandsbediening.ster()) {
-        basic.showArrow(ArrowNames.NorthWest)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CCW, 100)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CW, 100)
-        basic.pause(herhaal * 500)
-        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
-    }
-    if (IRcode == Afstandsbediening.hekje()) {
-        basic.showArrow(ArrowNames.NorthEast)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M1, Maqueen_V5.Dir.CW, 100)
-        Maqueen_V5.motorRun(Maqueen_V5.Motors.M2, Maqueen_V5.Dir.CCW, 100)
-        basic.pause(herhaal * 500)
-        Maqueen_V5.motorStop(Maqueen_V5.Motors.All)
-    }
-    if (IRcode == Afstandsbediening.ok()) {
-        basic.showIcon(IconNames.Yes)
-        music.play(music.stringPlayable("C5 A F F E G A B ", 264), music.PlaybackMode.UntilDone)
-    }
-    basic.showIcon(IconNames.Diamond)
 }
 function average (afstand: number) {
     lijst.pop()
